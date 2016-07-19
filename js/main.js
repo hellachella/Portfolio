c7d517caf1b857fe858e4a289a32b6b45c3ae17a
Michelle <vand0432@algonquinlive.com>
2016-07-19 11:02:20 -0400

    denture animation

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var $dentures = $('.dentures');
+var $subtext = $('.subtext');
+var $chelle = $('.chelle');
+
+$chelle.on('mouseover', function () {
+	$dentures.addClass('rollteeth');
+	$subtext.addClass('fadein');
+});
+
+$chelle.on('click', function () {
+	$dentures.addClass('rollteeth');
+	$subtext.addClass('fadein');
+});
+
+$chelle.on('animationend', function () {
+	$dentures.removeClass('rollteeth');
+	$subtext.removeClass('fadein');
+});
